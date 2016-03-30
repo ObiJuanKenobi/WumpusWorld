cbeb21cd0d7b5efe38eda82e11bcf61a517e6b7d
Juan Venegas <venegas@iastate.edu>
2016-03-30 09:26:32 -0500

    Some getters & setters for Tile

--- src/logic/Tile.java
+++ src/logic/Tile.java
@@ -1,13 +1,41 @@
 package logic;
 
+import java.util.ArrayList;
+
 public class Tile {
-	
-	public Tile() {
-		
+	private ArrayList<Percepts> percepts = new ArrayList<Percepts>();
+	private int X;
+	private int Y;
+	private Player p;
+	public Tile(int xPostion, int yPosition, ArrayList<Percepts> percepts) {
+		X = xPostion;
+		Y = yPosition;
+		this.percepts = percepts;
+	}
+
+	public Percepts[] getPercepts() {
+		return (Percepts[]) percepts.toArray();
+	}
+
+	public void setPercepts(ArrayList<Percepts> percepts) {
+		this.percepts = percepts;
+	}
+
+	public int getX() {
+		return X;
+	}
+
+	public void setX(int x) {
+		X = x;
+	}
+
+	public int getY() {
+		return Y;
+	}
+
+	public void setY(int y) {
+		Y = y;
 	}
 	
-	public boolean getFoo() {
-		return true;
-	}
 
 }
