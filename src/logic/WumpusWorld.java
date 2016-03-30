7fe99324f97fc73f41b517f76bdb35df617a6beb
Juan Venegas <venegas@iastate.edu>
2016-03-30 13:58:42 -0500

    Changed world to map

--- src/logic/WumpusWorld.java
+++ src/logic/WumpusWorld.java
@@ -5,26 +5,26 @@ import java.util.ArrayList;
 
 public class WumpusWorld {
 
-	private Tile[][] world;
+	private Tile[][] map;
 	private int playerX;
 	private int playerY;
 	private char playerOR;
 
 	// A hard coded map 4x4 map
 	public WumpusWorld() {
-		GenerateWorld();
+		Generatemap();
 	}
 
 	public WumpusWorld(int size) {
-		// Random world generation
+		// Random map generation
 	}
 
 	public Tile getTile(int x, int y) {
-		return world[x][y];
+		return map[x][y];
 	}
 
-	public Tile[][] getWorld() {
-		return world;
+	public Tile[][] getmap() {
+		return map;
 	}
 
 	public boolean move(int direction) {
@@ -89,11 +89,11 @@ public class WumpusWorld {
 	}
 
 	//like the map on the website
-	private void GenerateWorld() {
-		world = new Tile[4][4];
+	private void Generatemap() {
+		map = new Tile[4][4];
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
-				world[i][j] = new Tile(i, j);
+				map[i][j] = new Tile(i, j);
 			}
 		}
 		setUpObjectives();
@@ -104,22 +104,22 @@ public class WumpusWorld {
 	}
 
 	private void setUpPercepts() {
-		world[0][1].addPrecept(Percepts.Stench);
-		world[1][0].addPrecept(Percepts.Breeze);
-		world[1][2].addPrecept(Percepts.Stench);
-		world[1][2].addPrecept(Percepts.Breeze);
-		world[2][1].addPrecept(Percepts.Breeze);
-		world[2][3].addPrecept(Percepts.Breeze);
-		world[3][0].addPrecept(Percepts.Breeze);
-		world[3][2].addPrecept(Percepts.Breeze);
+		map[0][1].addPrecept(Percepts.Stench);
+		map[1][0].addPrecept(Percepts.Breeze);
+		map[1][2].addPrecept(Percepts.Stench);
+		map[1][2].addPrecept(Percepts.Breeze);
+		map[2][1].addPrecept(Percepts.Breeze);
+		map[2][3].addPrecept(Percepts.Breeze);
+		map[3][0].addPrecept(Percepts.Breeze);
+		map[3][2].addPrecept(Percepts.Breeze);
 	}
 
 	private void setUpObjectives() {
-		world[0][2].setObjective(Objectives.Pit);
-		world[0][2].setObjective(Objectives.Wumpus);
-		world[1][2].setObjective(Objectives.Gold);
-		world[2][2].setObjective(Objectives.Pit);
-		world[3][3].setObjective(Objectives.Pit);
-		world[1][3].setObjective(Objectives.Ladder);
+		map[0][2].setObjective(Objectives.Pit);
+		map[0][2].setObjective(Objectives.Wumpus);
+		map[1][2].setObjective(Objectives.Gold);
+		map[2][2].setObjective(Objectives.Pit);
+		map[3][3].setObjective(Objectives.Pit);
+		map[1][3].setObjective(Objectives.Ladder);
 	}
 }
