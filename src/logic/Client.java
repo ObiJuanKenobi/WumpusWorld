bf20f3e717a93145e7fb4d8112311f1558581edb
Juan Venegas <venegas@iastate.edu>
2016-04-16 12:16:30 -0500

    Added Some Server implementation

--- src/logic/Client.java
+++ src/logic/Client.java
@@ -1,5 +1,11 @@
 package logic;
 
 public class Client {
+	String hostName = "localHost";
+	int portNumber = 4444;
+	
+	public static void main(String[] args){
+		
+	}
 
 }
--- src/logic/Server.java
+++ src/logic/Server.java
@@ -1,5 +1,32 @@
 package logic;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+	String hostName = "localhost";
+	
+
+	public static void main(String[] args) {
+	int portNumber = 4444;
+	boolean listening = true;
+	
+		try(ServerSocket serverSocket = new ServerSocket(portNumber)){
+			while(listening){
+				//need to have 2 clients to do this
+				//TODO: have a better way of handling this
+				Socket client1 = serverSocket.accept();
+				Socket client2 = serverSocket.accept();
+				
+				new ServerThread(client1).start();
+				new ServerThread(client2).start();
+		
+			}
+		}catch(Exception e){
+			System.err.println("Could not create server socket");
+			System.exit(-1);
+		}
+
+	}
 
 }
