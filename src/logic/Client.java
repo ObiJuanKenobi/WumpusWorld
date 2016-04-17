deb2a3fe7a94d938b712509a55d08589a28cc8db
Juan Venegas <venegas@iastate.edu>
2016-04-17 08:23:35 -0500

    Added some client implementation

--- src/logic/Client.java
+++ src/logic/Client.java
@@ -1,11 +1,29 @@
 package logic;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
-	String hostName = "localHost";
-	int portNumber = 4444;
+	PrintWriter outToServer;
+	static BufferedReader fromServer;
+	
 	
 	public static void main(String[] args){
+		String hostName = "localHost";
+		int portNumber = 4444;
 		
+		try{
+			Socket clientSocket = new Socket(hostName, portNumber);
+			fromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream());
+			String fromServer, toServer;
+			
+			
+			
+		}catch(Exception e){
+			
+		}
 	}
 
 }
